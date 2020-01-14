6db5204b23a95fdcc7a027fc2a5169c6580ad25a
chadrbean <58306341+chadrbean@users.noreply.github.com>
2020-01-14 12:19:40 -0800

    Create es_bool_crunchbase.py

---
+++ es_bool_crunchbase.py
@@ -0,0 +1,58 @@
+from elasticsearch import Elasticsearch
+import json
+import sys
+import csv
+
+
+
+def find_linkedin_count():
+    es = Elasticsearch(['10.138.15.223', '10.138.15.220', '10.138.15.228'])
+    q = {"query":{"exists":{"field":"linkedin"}}}
+    search_object = q
+    result = es.count(
+        index='company-v1',
+        body=json.dumps(search_object),
+        request_timeout=30
+    )
+    return(result)
+def find_cb_count():
+    es = Elasticsearch(['10.138.15.223', '10.138.15.220', '10.138.15.228'])
+    q = {"query":{"exists":{"field":"crunchbase"}}}
+    search_object = q
+    result = es.count(
+        index='company-v1',
+        body=json.dumps(search_object),
+        request_timeout=30
+    )
+    return(result)
+
+def find_neither():
+    es = Elasticsearch(['10.138.15.223', '10.138.15.220', '10.138.15.228'])
+    q = {"query":{"bool":{"must":[{"bool":{"must_not":{"exists":{"field":"crunchbase"}}}},{"bool":{"must_not":{"exists":{"field":"linkedin"}}}}]}}}
+
+    
+    search_object = q
+    result = es.count(
+        index='company-v1',
+        body=json.dumps(search_object),
+        request_timeout=30
+    )
+    return(result)
+
+def create_csv():
+    with open("csv_out_files/company_LI_CB_Count.csv", "w") as fw:
+        found_linkedin_count = find_linkedin_count()
+        fieldnames = ["Type", 'Count']
+        csvw = csv.DictWriter(fw, fieldnames=fieldnames, delimiter='|')
+        csvw.writeheader()
+        csvw.writerow({"Type":"Linkedin", "Count":found_linkedin_count["count"]})
+        found_linkedin_count = find_cb_count()
+        csvw.writerow({"Type":"Crunchbase", "Count":found_linkedin_count["count"]})
+        found_find_neither = find_neither()
+        csvw.writerow({"Type":"No LI or CB Info", "Count":found_find_neither["count"]})
+    
+    
+
+
+store_data = create_csv()
+print(store_data)
